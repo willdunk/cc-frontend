03348d6537144beda4dbb02a4aa92482a3c8c632
William Dunkerley <will.eric.dunkerley@gmail.com>
2024-06-20 22:28:29 -0400

    fix(postLogout): Prevent withCredentials from being sent as the body

--- src/mutations/postLogout.ts
+++ src/mutations/postLogout.ts
@@ -4,5 +4,5 @@ import urlJoin from 'url-join';
 import { POST_LOGOUT } from '../constants/endpoints';
 
 export const postLogout = async () => {
-    return await axios.post(urlJoin(getEnv().CC_API, POST_LOGOUT), { withCredentials: true });
+    return await axios.post(urlJoin(getEnv().CC_API, POST_LOGOUT), {}, { withCredentials: true });
 };
